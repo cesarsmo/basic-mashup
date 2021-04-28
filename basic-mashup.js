75d8a31a3d033602a885f0f2ffa267ee3904b8aa
cesarsmo <cesar.moori@gmail.com>
2021-04-28 20:45:55 -0300

    v2

--- basic-mashup.js
+++ basic-mashup.js
@@ -36,32 +36,33 @@ script.onload = async () => {
 	const loginUrl = new URL(`${baseUrl}/login`);
 	loginUrl.searchParams.append('returnto', location.href);
 	loginUrl.searchParams.append('qlik-web-integration-id', config.webIntegrationId);
+
+
+	require( ["js/qlik"], function ( qlik ) {
+
+		qlik.on( "error", function ( error ) {
+			$( '#popupText' ).append( error.message + "<br>" );
+			$( '#popup' ).fadeIn( 1000 );
+		} );
+
+		$( "#closePopup" ).click( function () {
+			$( '#popup' ).hide();
+		} );
+
+		const app = qlik.openApp(config.appId);
+		app.getObject('CurrentSelections', 'CurrentSelections');
+
+		//callbacks -- inserted here --
+		//open apps -- inserted here --
+		// var app = qlik.openApp('4aef20d3-a3a7-4e93-9e65-70f11b624521', config);
+		// var app = qlik.openApp('Helpdesk Management.qvf', config);
+		// var app = qlik.openApp('999759c8-696c-4009-9546-0e658a9c6fdc', config);
+
+		//get objects -- inserted here --
+		app.getObject('QV03','JARjh');
+		app.getObject('QV02','jTuCwkB');
+		app.getObject('QV01','JsVPe');
+		app.getObject('QV04','PAppmU', {noSelections:"true"});
+		//create cubes and lists -- inserted here --
+	});
 }
-
-require( ["js/qlik"], function ( qlik ) {
-
-	qlik.on( "error", function ( error ) {
-		$( '#popupText' ).append( error.message + "<br>" );
-		$( '#popup' ).fadeIn( 1000 );
-	} );
-
-	$( "#closePopup" ).click( function () {
-		$( '#popup' ).hide();
-	} );
-
-    const app = qlik.openApp(config.appId);
-    app.getObject('CurrentSelections', 'CurrentSelections');
-
-	//callbacks -- inserted here --
-	//open apps -- inserted here --
-	// var app = qlik.openApp('4aef20d3-a3a7-4e93-9e65-70f11b624521', config);
-	// var app = qlik.openApp('Helpdesk Management.qvf', config);
-	// var app = qlik.openApp('999759c8-696c-4009-9546-0e658a9c6fdc', config);
-
-	//get objects -- inserted here --
-	app.getObject('QV03','JARjh');
-	app.getObject('QV02','jTuCwkB');
-	app.getObject('QV01','JsVPe');
-	app.getObject('QV04','PAppmU', {noSelections:"true"});
-	//create cubes and lists -- inserted here --
-});
