4dbde570feedfc906d3bae3f78335a31e22b5f32
cesarsmo <cesar.moori@gmail.com>
2021-04-28 22:39:34 -0300

    v2

--- basic-mashup.js
+++ basic-mashup.js
@@ -7,7 +7,7 @@
  */
 // var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/extensions" ) + 1 );
 
-var config = {
+let config = {
 		host: 'https://grupoitg-nordica.us.qlikcloud.com',
 		port: '443',
 		isSecure: true,
@@ -28,6 +28,7 @@ require.config( {
 
 
 require( ["js/qlik"], function ( qlik ) {
+
 	qlik.on( "error", function ( error ) {
 		$( '#popupText' ).append( error.message + "<br>" );
 		$( '#popup' ).fadeIn( 1000 );
