0cf68132acc0a6b357b9988aa80d9ccd88eceb81
cesarsmo <cesar.moori@gmail.com>
2021-04-28 22:46:30 -0300

    v2

--- basic-mashup.js
+++ basic-mashup.js
@@ -7,33 +7,31 @@
  */
 // var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/extensions" ) + 1 );
 
+// var config = {
+// 	host: window.location.hostname,
+// 	prefix: prefix,
+// 	port: window.location.port,
+// 	isSecure: window.location.protocol === "https:"
+// };
+
 let config = {
-		host: 'https://grupoitg-nordica.us.qlikcloud.com',
-		port: 443,
-		isSecure: true,
-		prefix: '/',
-		webIntegrationId: 'zQLeIH8-uf87QC9JyLRsdrdZpvhVlkli',
-		appId: '999759c8-696c-4009-9546-0e658a9c6fdc',
-};
+	host: 'grupoitg-nordica.us.qlikcloud.com',
+	prefix: '/',
+	port: 443,
+	isSecure: true,
+	webIntegrationId: 'zQLeIH8-uf87QC9JyLRsdrdZpvhVlkli'
+}
 
 require.config( {
-	baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources",
-	webIntegrationId: config.webIntegrationId
-} );		
-
-// build a single-sign on URL and return back here once completed:
-// const loginUrl = new URL((config.isSecure ? 'https://' : 'http://') + config.host+'/login');
-// loginUrl.searchParams.append('returnto', location.href);
-// loginUrl.searchParams.append('qlik-web-integration-id', config.webIntegrationId);
-
+    baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources",
+    webIntegrationId: config.webIntegrationId
+} );
 
 require( ["js/qlik"], function ( qlik ) {
-
 	qlik.on( "error", function ( error ) {
 		$( '#popupText' ).append( error.message + "<br>" );
 		$( '#popup' ).fadeIn( 1000 );
 	} );
-
 	$( "#closePopup" ).click( function () {
 		$( '#popup' ).hide();
 	} );
@@ -42,7 +40,7 @@ require( ["js/qlik"], function ( qlik ) {
 	//open apps -- inserted here --
 	// var app = qlik.openApp('4aef20d3-a3a7-4e93-9e65-70f11b624521', config);
 	// var app = qlik.openApp('Helpdesk Management.qvf', config);
-	var app = qlik.openApp(config.appId, config);
+	var app = qlik.openApp('999759c8-696c-4009-9546-0e658a9c6fdc', config);
 
 	//get objects -- inserted here --
 	app.getObject('QV03','JARjh');
